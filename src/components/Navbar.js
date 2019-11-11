97155fc6c56f6b263ffb730c6f8772a6f662d62d
Antonio <Memphis2Coder@gmail.com>
2019-11-11 12:44:06 -0600

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import logo from "../images/logo.svg";
+import { FaAlignRight } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+export default class Navbar extends Component {
+  state = {
+    isOpen: false
+  };
+  handleToggle = () => {
+    this.setState({ isOpen: !this.state.isOpen });
+  };
+  render() {
+    return (
+      <nav className="navbar navbar-light bg-light">
+        <div className="nav-header container">
+          <Link to="/">
+            <a className="logo">MY HOTEL</a>
+          </Link>
+          <button type="button" className="nav-btn" onClick={this.handleToggle}>
+            <FaAlignRight className="nav-icon" />
+          </button>
+        </div>
+        <ul className={this.state.isOpen ? "nav-links show-nav" : "nav-links"}>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/Rooms">Rooms</Link>
+          </li>
+        </ul>
+      </nav>
+    );
+  }
+}
---
+++ src/pages/Home.js
@@ -0,0 +1,6 @@
+import React from "react";
+import Hero from "../components/Hero";
+
+export default function Home() {
+  return <Hero>hello this is the home page</Hero>;
+}
